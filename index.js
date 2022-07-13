588a8bd925c93fac91f5feb2faf9032226b005f2
Hasan N <hsnnkb.dev@gmail.com>
2022-07-13 17:18:37 +0100

    Project initialisation
    Add html structure, css styling and get form elements in index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const postcode = document.getElementById("postcode");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
